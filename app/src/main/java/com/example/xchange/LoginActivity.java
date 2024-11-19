0ab129dd16998e13f34535e1ec6cb642e153e5c4
swkra <swkratisgiannoutsos@gmail.com>
2024-11-19 15:46:50 +0200

    Added the non-functional requirements

---
+++ app/src/main/java/com/example/xchange/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.example.xchange;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.login);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference();
+        myRef.setValue("Hello!");
+    }
+}
