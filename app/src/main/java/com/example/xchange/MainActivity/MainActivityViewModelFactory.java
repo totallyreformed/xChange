412e298d6addc625b775a3ce0b0b227cb22091f2
PPavlou <periklispavlou04@gmail.com>
2025-01-11 14:25:02 +0200

    fixed some code

--- app/src/main/java/com/example/xchange/MainActivity/MainActivityViewModelFactory.java
+++ app/src/main/java/com/example/xchange/MainActivity/MainActivityViewModelFactory.java
@@ -6,19 +6,22 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
+
 public class MainActivityViewModelFactory implements ViewModelProvider.Factory {
-
     private final Application application;
+    private final Context context;
 
-    public MainActivityViewModelFactory(Application application) {
+    public MainActivityViewModelFactory(Application application, Context context) {
         this.application = application;
+        this.context = context;
     }
 
     @NonNull
     @Override
     public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
         if (modelClass.isAssignableFrom(MainActivityViewModel.class)) {
-            return (T) new MainActivityViewModel(application);
+            return (T) new MainActivityViewModel(application, context);
         }
         throw new IllegalArgumentException("Unknown ViewModel class");
     }
